fa65fac1f92cdf47cb51544d00b58bb3a55cb19d
Arunachalam13 <karunachalam406@gmail.com>
2025-04-25 07:27:24 +0530

    Added the Asp.Net core MVC project, EF, and BrandController.cs.

---
+++ TopSpeed.Web/TopSpeed.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopSpeed.Web.Models;
+
+namespace TopSpeed.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
